ad865694db23c65e0fe1c5c13f28adbcef26543a
FL0R1AN <fl0r1an@waechter.koeln>
2023-11-21 21:19:09 +0100

    feat: add card modal

---
+++ src/components/ModalCard.vue
@@ -0,0 +1,87 @@
+<template>
+  <ion-page ref="page">
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>App</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content class="ion-padding">
+      <ion-button id="open-modal" class="center-button">{{ title }}</ion-button>
+
+      <ion-modal
+        ref="modal"
+        trigger="open-modal"
+        :presenting-element="presentingElement"
+      >
+        <ion-header>
+          <ion-toolbar>
+            <ion-title>Modal</ion-title>
+            <ion-buttons slot="start">
+              <ion-button @click="dismiss()">Close</ion-button>
+            </ion-buttons>
+          </ion-toolbar>
+        </ion-header>
+
+        <ion-content class="ion-padding">
+          <ion-range
+            label-placement="start"
+            label="Label at the Start"
+          ></ion-range>
+
+          <br />
+
+          <ion-range label-placement="end" label="Label at the End"></ion-range>
+
+          <br />
+
+          <ion-range
+            label-placement="fixed"
+            label="Fixed Width Label"
+          ></ion-range>
+
+          <br />
+
+          <ion-range
+            label-placement="stacked"
+            label="Stacked Label"
+          ></ion-range>
+        </ion-content>
+      </ion-modal>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import {
+  IonButtons,
+  IonButton,
+  IonModal,
+  IonHeader,
+  IonContent,
+  IonToolbar,
+  IonTitle,
+  IonPage,
+  modalController
+} from '@ionic/vue'
+
+defineProps<{
+  title: string
+}>()
+
+const presentingElement = null
+
+const dismiss = () => {
+  modalController.dismiss()
+}
+</script>
+
+<style scoped>
+.center-button {
+  text-align: center;
+  position: absolute;
+  left: 25%;
+  right: 25%;
+  top: 50%;
+  transform: translateY(-50%);
+}
+</style>
--- src/views/TabsPage.vue
+++ src/views/TabsPage.vue
@@ -9,13 +9,13 @@
         </ion-tab-button>
 
         <ion-tab-button tab="tab2" href="/tabs/tab2">
-          <ion-icon aria-hidden="true" :icon="ellipse" />
-          <ion-label>Tab 2</ion-label>
+          <ion-icon aria-hidden="true" :icon="chevronUpOutline" />
+          <ion-label>Card</ion-label>
         </ion-tab-button>
 
         <ion-tab-button tab="tab3" href="/tabs/tab3">
-          <ion-icon aria-hidden="true" :icon="square" />
-          <ion-label>Tab 3</ion-label>
+          <ion-icon aria-hidden="true" :icon="chevronDownOutline" />
+          <ion-label>Sheet</ion-label>
         </ion-tab-button>
       </ion-tab-bar>
     </ion-tabs>
@@ -32,5 +32,10 @@ import {
   IonPage,
   IonRouterOutlet
 } from '@ionic/vue'
-import { ellipse, square, triangle } from 'ionicons/icons'
+import {
+  chevronUpOutline,
+  chevronDownOutline,
+  square,
+  triangle
+} from 'ionicons/icons'
 </script>
