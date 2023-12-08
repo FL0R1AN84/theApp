bc65f93abd3f5f33422c1315e8a53a0b17c85d5e
FL0R1AN <fl0r1an@waechter.koeln>
2023-12-08 23:24:24 +0100

    feat: add click counter

--- src/components/CalculateAlert.vue
+++ src/components/CalculateAlert.vue
@@ -1,8 +1,15 @@
 <template>
   <HeaderDefault title="Calc" />
-  <ButtonDefault title="Calculate and Alert" />
+  <ion-item>
+    <ButtonDefault title="Calculate and Alert" @click="clickCounter++" />
+    <ion-badge slot="end">{{ clickCounter }}</ion-badge>
+  </ion-item>
 </template>
 
 <script setup lang="ts">
+import { IonBadge } from '@ionic/vue'
+import { ref } from 'vue'
 import ButtonDefault from './elements/ButtonDefault.vue'
+
+const clickCounter = ref(0)
 </script>
