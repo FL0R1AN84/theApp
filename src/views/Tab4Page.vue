4cd13df24e73609b4fd39305dc2998f2c90aea5e
FL0R1AN <fl0r1an@waechter.koeln>
2023-12-21 20:48:59 +0100

    feat: expand tab bar
    Create two new pages as placeholders for upcoming

--- src/views/Tab4Page.vue
+++ src/views/Tab4Page.vue
@@ -1,26 +1,26 @@
 <template>
   <ion-page>
-    <HeaderDefault title="Mapbox" />
+    <HeaderDefault title="Calc" />
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Mapbox</ion-title>
+          <ion-title size="large">Calc</ion-title>
         </ion-toolbar>
       </ion-header>
       <ion-content class="ion-padding">
-        <ion-text>Mapbox is coming soon</ion-text>
+        <CalculateAlert title="Calc&ALert" trigger="open-card-modal" />
       </ion-content>
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
+import CalculateAlert from '@/components/CalculateAlert.vue'
 import HeaderDefault from '@/components/elements/HeaderDefault.vue'
 import {
   IonContent,
   IonHeader,
   IonPage,
-  IonText,
   IonTitle,
   IonToolbar
 } from '@ionic/vue'
