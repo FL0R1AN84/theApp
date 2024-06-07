bf06ba3ed9e0aa5bafcd38e5c7ac51f32388e3a8
FL0R1AN <fl0r1an@waechter.koeln>
2024-06-08 00:47:41 +0200

    refactor: use setup script

--- src/components/MapBox.vue
+++ src/components/MapBox.vue
@@ -2,33 +2,24 @@
   <div ref="mapContainer" class="map-container"></div>
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
 import { onMounted, ref } from 'vue'
 import mapboxgl from 'mapbox-gl'
 
-export default {
-  name: 'MapBox',
-  setup() {
-    const mapContainer = ref(null)
-    const map = ref(null)
+const mapContainer = ref(null)
+const map = ref(null)
 
-    onMounted(() => {
-      mapboxgl.accessToken =
-        'pk.eyJ1IjoiZmwwcjFhbjg0IiwiYSI6ImNseDU4dHkyYzFjdG8ya3M0cWt5NHYxeTQifQ.BOPdowVpzfpSn6qox_-nWw'
+onMounted(() => {
+  mapboxgl.accessToken =
+    'pk.eyJ1IjoiZmwwcjFhbjg0IiwiYSI6ImNseDU4dHkyYzFjdG8ya3M0cWt5NHYxeTQifQ.BOPdowVpzfpSn6qox_-nWw'
 
-      map.value = new mapboxgl.Map({
-        container: mapContainer.value,
-        style: 'mapbox://styles/mapbox/standard',
-        center: [6.958735655501745, 50.94179850699188],
-        zoom: 12
-      })
-    })
-
-    return {
-      mapContainer
-    }
-  }
-}
+  map.value = new mapboxgl.Map({
+    container: mapContainer.value,
+    style: 'mapbox://styles/mapbox/standard',
+    center: [6.958735655501745, 50.94179850699188],
+    zoom: 12
+  })
+})
 </script>
 
 <style scoped>
