1c7bf86b9b399916ecc00bd245f5f510b93bd4c0
FL0R1AN <fl0r1an@waechter.koeln>
2024-06-08 00:45:47 +0200

    feat: add MapBox

---
+++ src/components/MapBox.vue
@@ -0,0 +1,39 @@
+<template>
+  <div ref="mapContainer" class="map-container"></div>
+</template>
+
+<script lang="ts">
+import { onMounted, ref } from 'vue'
+import mapboxgl from 'mapbox-gl'
+
+export default {
+  name: 'MapBox',
+  setup() {
+    const mapContainer = ref(null)
+    const map = ref(null)
+
+    onMounted(() => {
+      mapboxgl.accessToken =
+        'pk.eyJ1IjoiZmwwcjFhbjg0IiwiYSI6ImNseDU4dHkyYzFjdG8ya3M0cWt5NHYxeTQifQ.BOPdowVpzfpSn6qox_-nWw'
+
+      map.value = new mapboxgl.Map({
+        container: mapContainer.value,
+        style: 'mapbox://styles/mapbox/standard',
+        center: [6.958735655501745, 50.94179850699188],
+        zoom: 12
+      })
+    })
+
+    return {
+      mapContainer
+    }
+  }
+}
+</script>
+
+<style scoped>
+.map-container {
+  width: 100%;
+  height: 100vh;
+}
+</style>
