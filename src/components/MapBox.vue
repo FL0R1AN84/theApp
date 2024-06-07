7df2423c45576adb49f60c99dd503b16a71329ed
FL0R1AN <fl0r1an@waechter.koeln>
2024-06-08 00:50:53 +0200

    fix: type error

--- src/components/MapBox.vue
+++ src/components/MapBox.vue
@@ -6,15 +6,15 @@
 import { onMounted, ref } from 'vue'
 import mapboxgl from 'mapbox-gl'
 
-const mapContainer = ref(null)
-const map = ref(null)
+const mapContainer = ref<HTMLElement | null>(null)
+const map = ref<mapboxgl.Map | null>(null)
 
 onMounted(() => {
   mapboxgl.accessToken =
     'pk.eyJ1IjoiZmwwcjFhbjg0IiwiYSI6ImNseDU4dHkyYzFjdG8ya3M0cWt5NHYxeTQifQ.BOPdowVpzfpSn6qox_-nWw'
 
   map.value = new mapboxgl.Map({
-    container: mapContainer.value,
+    container: mapContainer.value!,
     style: 'mapbox://styles/mapbox/standard',
     center: [6.958735655501745, 50.94179850699188],
     zoom: 12
