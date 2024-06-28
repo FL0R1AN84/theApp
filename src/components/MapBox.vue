aca76e9530be0113bdf9100c74066a33806280bc
FL0R1AN <fl0r1an@waechter.koeln>
2024-06-28 23:54:55 +0200

    feat: add mapboxgl library

--- src/components/MapBox.vue
+++ src/components/MapBox.vue
@@ -1,30 +1,16 @@
 <template>
-  <div ref="mapContainer" class="map-container"></div>
+  <MapboxMap
+    :center="mapCenter"
+    :zoom="11"
+    access-token="pk.eyJ1IjoiZmwwcjFhbjg0IiwiYSI6ImNseDU4dHkyYzFjdG8ya3M0cWt5NHYxeTQifQ.BOPdowVpzfpSn6qox_-nWw"
+    map-style="mapbox://styles/mapbox/standard"
+    style="height: 100vh; width: 100%"
+  />
 </template>
 
-<script lang="ts" setup>
-import { onMounted, ref } from 'vue'
-import mapboxgl from 'mapbox-gl'
+<script setup>
+import { ref } from 'vue'
+import { MapboxMap } from '@studiometa/vue-mapbox-gl'
 
-const mapContainer = ref<HTMLElement | null>(null)
-const map = ref<mapboxgl.Map | null>(null)
-
-onMounted(() => {
-  mapboxgl.accessToken =
-    'pk.eyJ1IjoiZmwwcjFhbjg0IiwiYSI6ImNseDU4dHkyYzFjdG8ya3M0cWt5NHYxeTQifQ.BOPdowVpzfpSn6qox_-nWw'
-
-  map.value = new mapboxgl.Map({
-    container: mapContainer.value!,
-    style: 'mapbox://styles/mapbox/standard',
-    center: [6.958735655501745, 50.94179850699188],
-    zoom: 12
-  })
-})
+const mapCenter = ref([6.958735655501745, 50.94179850699188])
 </script>
-
-<style scoped>
-.map-container {
-  width: 100%;
-  height: 100vh;
-}
-</style>
