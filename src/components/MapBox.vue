3b05fbcfcb36108bb28a013361a01e0db63195b4
FL0R1AN <fl0r1an@waechter.koeln>
2024-08-24 20:08:39 +0200

    feat: add get my position button  - refactor:  show barcode scanner error toast massage on bottom

--- src/components/MapBox.vue
+++ src/components/MapBox.vue
@@ -5,13 +5,15 @@
     access-token="pk.eyJ1IjoiZmwwcjFhbjg0IiwiYSI6ImNseDU4dHkyYzFjdG8ya3M0cWt5NHYxeTQifQ.BOPdowVpzfpSn6qox_-nWw"
     map-style="mapbox://styles/mapbox/standard"
     style="height: 100vh; width: 100%"
-  />
+  >
+    <MapboxGeolocateControl />
+  </MapboxMap>
 </template>
 
 <script lang="ts" setup>
 import 'mapbox-gl/dist/mapbox-gl.css'
 import { ref } from 'vue'
-import { MapboxMap } from '@studiometa/vue-mapbox-gl'
+import { MapboxGeolocateControl, MapboxMap } from '@studiometa/vue-mapbox-gl'
 
 const mapCenter = ref([6.958735655501745, 50.94179850699188])
 </script>
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,4 +1,6 @@
 /// <reference types="vite/client" />
 declare module '@studiometa/vue-mapbox-gl' {
   export const MapboxMap: any
+
+  export class MapboxGeolocateControl {}
 }
